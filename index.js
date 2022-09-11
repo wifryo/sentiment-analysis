d3e820511eeff9a43ba02151c5616a9bbd8a72b3
William Young <wifryo@gmail.com>
2022-09-11 09:37:21 +0200

    Tidy syntax

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import vader from 'vader-sentiment';
+import { stdout } from 'node:process';
 import MonkeyLearn from 'monkeylearn';
-import { stdout } from 'process';
+import vader from 'vader-sentiment';
 
 const args = process.argv.slice(2);
 let inputText = '';
@@ -11,9 +11,9 @@ for (let i = 0; args[i]; i++) {
 
 const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(inputText);
 
-let pos = `${intensity.pos * 100}%`;
-let neu = `${intensity.neu * 100}%`;
-let neg = `${intensity.neg * 100}%`;
+const pos = `${intensity.pos * 100}%`;
+const neu = `${intensity.neu * 100}%`;
+const neg = `${intensity.neg * 100}%`;
 
 console.log(
   '\n - - - - - - - - - \n\nAnalysis 1 from vader-sentiment\n\nYour text has the following sentiment:',
@@ -23,9 +23,9 @@ console.log(`${neu} neutral`);
 console.log(`${neg} negative`);
 
 const ml = new MonkeyLearn('272375b3a8174c3bdd859015b0fca784ce545c37');
-let model_id = 'cl_pi3C7JiL';
-let data = [inputText];
-ml.classifiers.classify(model_id, data).then((res) => {
+const id = 'cl_pi3C7JiL';
+const data = [inputText];
+ml.classifiers.classify(id, data).then((res) => {
   stdout.write(
     '\n - - - - - - - - - \n\nAnalysis 2 from MonkeyLearn: \n\nYour text has a sentiment of ',
   );
