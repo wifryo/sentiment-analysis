1c6e8a83c48cf4850f26b7bd96edc5e94398a841
William <wifryo@gmail.com>
2022-09-11 09:06:24 +0200

    Implement 2 methods of sentiment analysis

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 import vader from 'vader-sentiment';
+import MonkeyLearn from 'monkeylearn';
+import { stdout } from 'process';
 
 const args = process.argv.slice(2);
 let inputText = '';
@@ -13,7 +15,21 @@ let pos = `${intensity.pos * 100}%`;
 let neu = `${intensity.neu * 100}%`;
 let neg = `${intensity.neg * 100}%`;
 
-console.log('\nYour text has the following sentiment: \n');
+console.log(
+  '\n - - - - - - - - - \n\nAnalysis 1 from vader-sentiment\n\nYour text has the following sentiment:',
+);
 console.log(`${pos} positive`);
 console.log(`${neu} neutral`);
 console.log(`${neg} negative`);
+
+const ml = new MonkeyLearn('272375b3a8174c3bdd859015b0fca784ce545c37');
+let model_id = 'cl_pi3C7JiL';
+let data = [inputText];
+ml.classifiers.classify(model_id, data).then((res) => {
+  stdout.write(
+    '\n - - - - - - - - - \n\nAnalysis 2 from MonkeyLearn: \n\nYour text has a sentiment of ',
+  );
+  stdout.write(res.body[0].classifications[0].tag_name.toLowerCase());
+  stdout.write(', with a confidence of ');
+  console.log(res.body[0].classifications[0].confidence);
+});
